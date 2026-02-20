43d31c252c1bc753102d3bb7a186ad6973cbb98b
Diego-Martin-404 <up230346@alumnos.upa.edu.mx>
2026-02-20 17:18:43 -0600

    Update Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,51 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\n ->
+            if n >= 7 then
+                Approved
+
+            else if n >= 0 then
+                Failed
+
+            else
+                Pending
+        )
+        grades
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
